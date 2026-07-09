6766f97f06d6f6b709e9feb34de6ad74d6084b59
Mr-VISGH <mrvisgh2.0@gmail.com>
2026-07-09 20:34:11 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,26 @@
 plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
+
+android {
+    namespace = "com.vistaplayer"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.vistaplayer"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    val media3 = "1.7.1"
